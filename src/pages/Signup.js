ba7d900836761a4f1419340d4078e567c1048004
Maiiadel <Maiadel20014@gmail.com>
2023-02-28 16:01:52 +0200

    sign up sha8al ward

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,13 +2,37 @@ import { Link } from "react-router-dom";
 import Header from "../Components/Header";
 import './Login.css';
 import Swal from 'sweetalert2/dist/sweetalert2.js'
+import { useState } from "react";
+import axios from "axios";
 function Signup(){
- const adduser=()=>{
-  Swal.fire({
-    icon: 'success',
-    title: 'Successfully Signed Up'
+  let [FName,setFName]=useState('');
+  let [LName,setLName]=useState('');
+  let [username,setUsername]=useState('');
+  let [password,setPassword]=useState('');
+  let [Role,setRole]=useState('');
+
+ const adduser=(e)=>{
+  e.preventDefault();
+  axios.post("http://localhost:8000/users",
+  {FName,LName,username,password,Role}).then((data)=>{console.log(data);});
+Swal.fire({
+    title: 'Successfully Signed Up',
+    icon: 'success'
   }).then()
-  
+
+  // Swal.fire({
+  //   title: `Project " ${Name}" added successfully `,
+  //   icon: "info",
+  //   showDenyButton: false,
+  //   showConfirmButton:true,
+  //   confirmButtonText:'OK'
+  // }).then((result) => {
+  //   if (result.isConfirmed) {
+  //     navigate('/Projects');
+  //   } else{
+  //     Swal.fire('Project is not added', '', 'info')
+  //   }
+  // })
  }
     return(
     <>
@@ -22,12 +46,13 @@ function Signup(){
             type="text"
             className="form-control"
             placeholder="First name"
+            onChange={(e)=>{setFName(e.target.value);}}
           />
         </div>
 
         <div className="mb-3">
           <label>Last name: </label>
-          <input type="text" className="form-control" placeholder="Last name" />
+          <input type="text" className="form-control" placeholder="Last name" onChange={(e)=>{setLName(e.target.value);}}/>
         </div>
 
         <div className="mb-3">
@@ -36,6 +61,7 @@ function Signup(){
             type="email"
             className="form-control"
             placeholder="Enter email"
+            onChange={(e)=>{setUsername(e.target.value);}}
           />
         </div>
 
@@ -45,19 +71,21 @@ function Signup(){
             type="password"
             className="form-control"
             placeholder="Enter password"
+            onChange={(e)=>{setPassword(e.target.value);}}
           />
         </div>
 
         <div className="mb-3">
           <label>Role: </label>
           <br></br>
-          <select  name="Role" id="role" placeholder="choose role">
-    <option value="BA">Business Analyst</option>
-    <option value="stud">Student</option>
-    <option value="PO">Product Owner</option>
-    <option value="other">other</option>
-  </select>
+          <select  name="Role" id="role" placeholder="choose role" onChange={(e)=>{setRole(e.target.value)}}>
+          <option value="other">other</option>
+          <option value="BA">Business Analyst</option>
+          <option value="stud">Student</option>
+          <option value="PO">Product Owner</option>
+           </select>
         </div>
+
         <div className="mb-3">
           <label>Other:</label>
           <input
@@ -72,7 +100,7 @@ function Signup(){
           </button>
         </div>
         <p className="forgot-password text-right">
-          Already registered <Link to="/login">sign in?</Link>
+          Already registered <Link to="/login">Sign in?</Link>
         </p>
       </form>
     </>
